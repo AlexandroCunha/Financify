d5e4c37b65886ff9571929c3365b86fd3739706a
alexandro cunha <alexandrocunha4@gmail.com>
2023-10-24 14:47:53 -0300

    atualização nas rotas

--- src/config/routes/router.js
+++ src/config/routes/router.js
@@ -3,10 +3,10 @@ const { cadastrarUsuario, login, detalharUsuario, atualizarUsuario, deletarTrans
 const { transacoesDoUsuario, detalharTransacao, cadastrarTransacao, atualizarTransacao } = require('../controllers/transacoes')
 const { listarCategorias } = require('../controllers/categorias')
 const validarLogin = require('../middleware/validarlogin')
-const validarParaAtualizarUsuario = require('../middleware/validarAtualizarUsuario')
-const validarParaCadastrarUsuario = require('../middleware/validarCadastrarUsuario')
+const validarAtualizarUsuario = require('../middleware/validarAtualizarUsuario')
+const validarCadastrarUsuario = require('../middleware/validarCadastrarUsuario')
 const validarTransacaoParaUsuarioLogado = require('../middleware/ValidarSeExisteTransacaoParaUsuarioLogado')
-const validarParaAtualizarTransacao = require('../middleware/validarAtualizarTransacao')
+const validarAtualizarTransacao = require('../middleware/validarAtualizarTransacao')
 
 
 
@@ -18,12 +18,12 @@ rotas.post('/login', login)
 rotas.use(validarLogin)
 
 rotas.get('/usuario', detalharUsuario)
-rotas.put('/usuarios', validarParaAtualizarUsuario, atualizarUsuario)
+rotas.put('/usuarios', validarAtualizarUsuario, atualizarUsuario)
 rotas.get('/categoria', listarCategorias)
 rotas.get('/transacao', transacoesDoUsuario)
 rotas.get('/transacao/:id', validarTransacaoParaUsuarioLogado, detalharTransacao)
-rotas.post('/transacao', validarParaCadastrarUsuario, cadastrarTransacao)
-rotas.put('/transacao/:id', validarParaAtualizarTransacao, atualizarTransacao)
+rotas.post('/transacao', validarCadastrarUsuario, cadastrarTransacao)
+rotas.put('/transacao/:id', validarAtualizarTransacao, atualizarTransacao)
 rotas.delete('/transacao/:id', validarTransacaoParaUsuarioLogado, deletarTransacao)
 
 
